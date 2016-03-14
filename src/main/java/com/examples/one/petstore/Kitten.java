ae7eeb6d9289ae851c845d3ffaf273761bdc0c7e
Jonathan Borg <jonathan.borg.14@um.edu.mt>
2016-03-14 09:11:42 +0100

    updated kitten.java with getters and setters

--- src/main/java/com/examples/one/petstore/Kitten.java
+++ src/main/java/com/examples/one/petstore/Kitten.java
@@ -10,5 +10,38 @@ package com.examples.one.petstore;
  * @author jonathan
  */
 public class Kitten {
+    int id;
+    String name;
+    String breed;
+    boolean sensitiveToFeeding;
     
+    public void Kitten(String name1, int id1, String breed1){
+        name = name1;
+        id = id1;
+        breed = breed1;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public void setBreed(String breed){
+        this.breed = breed;
+    }    
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public String getBreed(){
+        return breed;
+    }    
 }
